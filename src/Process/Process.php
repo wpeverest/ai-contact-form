f9d1f926abb5f517e888061c11869425ddb37f8a
unknown <elendoff11@gmail.com>
2023-07-25 08:29:04 +0545

    Sentimental analysis done

--- src/Process/Process.php
+++ src/Process/Process.php
@@ -38,32 +38,22 @@ class Process {
 	public function process_filter( $form_fields, $entry, $form_data ) {
 		foreach ( $form_data['form_fields'] as $key => $field ) {
 			if ( array_key_exists( 'ai_input', $field ) ) {
-					$ai_prompt = $field['ai_input'];
-					preg_match_all( '/\{field_id="(.+?)"\}/', $ai_prompt, $ids );
-
-				if ( ! empty( $ids[1] ) ) {
-					foreach ( $ids[1] as $key => $field_id ) {
-						$mixed_field_id = explode( '_', $field_id );
-						if ( count( $mixed_field_id ) > 1 && ! empty( $form_fields[ $mixed_field_id[1] ] ) ) {
-							$providers = get_option( 'everest_forms_openai_settings', array() );
-							$api_key   = ! empty( $providers['api_key'] ) ? $providers['api_key'] : '';
-							$response  = new API( $api_key );
-							$data      = array(
-								'messages'    => array(
-									array(
-										'role'    => 'user',
-										'content' => apply_filters( 'everest_forms_process_smart_tags', $ai_prompt, $form_data, $form_fields ),
-									),
-								),
-								'temperature' => 0.5,
-							);
-							$content   = $response->send_openai_request( 'chat/completions', $data );
-							lg( $content );
-							// $message   = isset( $content['choices'][0]['message']['content'] ) ? esc_html( $content['choices'][0]['message']['content'] ) :
-
-						}
-					}
-				}
+					$ai_prompt                    = $field['ai_input'];
+					$providers                    = get_option( 'everest_forms_openai_settings', array() );
+					$api_key                      = ! empty( $providers['api_key'] ) ? $providers['api_key'] : '';
+					$response                     = new API( $api_key );
+					$data                         = array(
+						'messages'    => array(
+							array(
+								'role'    => 'user',
+								'content' => apply_filters( 'everest_forms_process_smart_tags', $ai_prompt, $form_data, $form_fields ),
+							),
+						),
+						'temperature' => 0.5,
+					);
+					$content                      = $response->send_openai_request( 'chat/completions', $data );
+					$message                      = isset( $content['choices'][0]['message']['content'] ) ? esc_html( $content['choices'][0]['message']['content'] ) : '';
+					$form_fields[ $key ]['value'] = $message;
 			}
 		}
 		return $form_fields;
