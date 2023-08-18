d44925d4f8ac6ca24a1dcb42e1cc131fd6272207
unknown <elendoff11@gmail.com>
2023-08-18 13:11:57 +0545

    updated

--- src/Admin/Settings.php
+++ src/Admin/Settings.php
@@ -32,20 +32,20 @@ class Settings extends \EVF_Settings_Page {
 		 */
 	public function get_settings() {
 		$settings = apply_filters(
-			'everest_forms_geolocation_settings',
+			'everest_forms_ai_settings',
 			array(
 				array(
 					'title' => esc_html__( 'AI', 'ai-contact-form' ),
 					'type'  => 'title',
 					/* translators: %1$s - Ai docs url */
-					'desc'  => sprintf( __( '<p>Everest Forms AI offers Chatbot functionality, Email Prompt assistance, and the capability for field analysis.</p><p>Get detailed documentation on  integrating<a href="%1$s" target="_blank"> Everest Forms AI.</a></p>', 'ai-contact-form' ), 'https://docs.wpeverest.com/everest-forms/docs/ai/' ),
+					'desc'  => sprintf( __( '<p>Everest Forms AI offers Chatbot functionality, Email Prompt assistance, and the capability for field analysis.</p><p>Get detailed documentation on  integrating<a href="%1$s" target="_blank"> AI.</a></p>', 'ai-contact-form' ), 'https://docs.everestforms.net/docs/ai/' ),
 					'id'    => 'everest_forms_ai_options',
 				),
 				array(
 					'title'    => esc_html__( 'AI API Key', 'ai-contact-form' ),
 					'type'     => 'text',
 					/* translators: %1$s - Google API docs url */
-					'desc'     => sprintf( esc_html__( 'Please enter your API key of AI. <a href="%1$s" target="_blank">Learn More</a>', 'ai-contact-form' ), esc_url( 'https://docs.wpeverest.com/everest-forms/docs/ai/' ) ),
+					'desc'     => sprintf( esc_html__( 'Please enter your API key of AI. <a href="%1$s" target="_blank">Learn More</a>', 'ai-contact-form' ), esc_url( 'https://docs.everestforms.net/docs/ai/' ) ),
 					'id'       => 'everest_forms_ai_api_key',
 					'default'  => '',
 					'desc_tip' => true,
